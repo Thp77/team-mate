9a9390dd1af644125d9b0aef302e805ebc8d12cb
unknown <denis_devillers_7@hotmail.com>
2023-09-21 17:59:49 +0200

    affichage d'un carrousel des 3 derniere Team créer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Repository\TeamRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
+//Activer si on ne veux plus afficher les Teams sur la page d'accueil //
+
+// class HomeController extends AbstractController
+// {
+//     #[Route('/', 'home.index', methods: ['GET'])]
+
+//     public function index(): Response
+//     {
+//         return $this->render('pages/home.html.twig');
+//     }
+// }
+
+//Controlleur pour afficher des articles sur la page d'accueil //
+
 class HomeController extends AbstractController
 {
     #[Route('/', 'home.index', methods: ['GET'])]
 
-    public function index(): Response
+    public function index(TeamRepository $teamRepository): Response
     {
-        return $this->render('pages/home.html.twig');
+        return $this->render('pages/home.html.twig',
+    ['teams' => $teamRepository->findPublicTeam(3)]);
     }
 }
